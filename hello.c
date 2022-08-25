e53983eff533cab0bdf5dfa6c92c33d7b7951e83
gcammack <Gage Cammack>
2022-08-25 13:30:04 +0000

    initial commit

---
+++ hello.c
@@ -0,0 +1,18 @@
+/**
+ * Author: Gage Cammack
+ *         gcammack2@huskers.unl.edu
+ * Date: 2022/08/25
+ *
+ * A simple hello world program in C
+ *
+ */
+#include <stdlib.h>
+#include <stdio.h>
+
+int main(int argc, char **argv) {
+
+  printf("Hello World!\n");
+  printf("Gage Cammack\n");
+
+  return 0;
+}
